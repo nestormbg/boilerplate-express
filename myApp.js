d2028034427a0285d01dcd25489aece16f06f699
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-20 08:50:03 -0300

    serve html file at root path

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
