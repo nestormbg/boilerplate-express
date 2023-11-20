ee6a464f2794258e120d8de225350d7deba9a4ff
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-20 09:15:17 -0300

    serve json data into /json path

--- myApp.js
+++ myApp.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/json', (req, res) => {
+  let data = {
+    message: "Hello json"
+  };
+  res.json(data);
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 
