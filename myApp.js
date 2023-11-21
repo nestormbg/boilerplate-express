23a07a92eac56241abc873788dc80c8fcc44341b
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-21 01:01:08 -0300

    fix object name

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${re.path} - ${req.ip}`);
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
